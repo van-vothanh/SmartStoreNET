3395bccad0385b645e816fb446e4f3469a9f5e48
Version Upgrade <n/a>
2025-09-22 07:27:20 +0000

    Upgrade to .NET 8 on Iteration 12

--- src/Libraries/SmartStore.Core/Domain/Media/Download.cs
+++ src/Libraries/SmartStore.Core/Domain/Media/Download.cs
@@ -15,7 +15,6 @@ namespace SmartStore.Core.Domain.Media
         /// Gets or sets a GUID
         /// </summary>
         [DataMember]
-        [Index]
         public Guid DownloadGuid { get; set; }
 
         /// <summary>
@@ -34,14 +33,12 @@ namespace SmartStore.Core.Domain.Media
         /// Gets or sets a value indicating whether the entity transient/preliminary
         /// </summary>
         [DataMember]
-        [Index("IX_UpdatedOn_IsTransient", 1)]
         public bool IsTransient { get; set; }
 
         /// <summary>
         /// Gets or sets the date and time of instance update
         /// </summary>
         [DataMember]
-        [Index("IX_UpdatedOn_IsTransient", 0)]
         public DateTime UpdatedOnUtc { get; set; }
 
         /// <summary>
@@ -60,14 +57,12 @@ namespace SmartStore.Core.Domain.Media
         /// Gets or sets a value indicating the corresponding entity id
         /// </summary>
         [DataMember]
-        [Index("IX_EntityId_EntityName", 0)]
         public int EntityId { get; set; }
 
         /// <summary>
         /// Gets or sets a value indicating the corresponding entity name
         /// </summary>
         [DataMember]
-        [Index("IX_EntityId_EntityName", 1)]
         [StringLength(100)]
         public string EntityName { get; set; }
 
